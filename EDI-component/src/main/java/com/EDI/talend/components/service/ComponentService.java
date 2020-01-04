61908ab902705b42cbad283f19652273d38cb6e6
MAADA Loukmane <36376593+PengaloGit@users.noreply.github.com>
2020-01-04 14:13:31 +0100

    Update ComponentService.java

--- EDI-component/src/main/java/com/EDI/talend/components/service/ComponentService.java
+++ EDI-component/src/main/java/com/EDI/talend/components/service/ComponentService.java
@@ -12,11 +12,10 @@ import java.text.SimpleDateFormat;
 public class ComponentService {
 
     public void fileTreatment(Record defaultInput, Error errors)  throws Exception{
-        String startDate = defaultInput.getString("DateDepart");
-        String endDate = defaultInput.getString("DateLivraison");
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        if(!sdf.parse(startDate).before(sdf.parse(endDate))){
-            errors.setWorkflowError("invalid date error");
+        //Here goes the treatment code
+        
+        //Setting Error
+        errors.setWorkflowError("my error");
         }
 
     }
